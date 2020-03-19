52b961dc1be1f35252f48722342cb7c86de9149e
Николай Фадеев <nikolay.fadeev.96@bk.ru>
2020-03-19 15:00:21 +0500

    ustranil bug pobedy

--- src/App.js
+++ src/App.js
@@ -58,9 +58,9 @@ class App extends React.Component {
         })
 
         if (cell[index] === null) {
-            cell[index] = (this.state.count % 2 === 0) ? "x" : 0
-            this.setState({
-                coutn: this.state.count++,
+                cell[index] = (this.state.count % 2 === 0) ? "x" : 0
+                this.setState({
+                count: this.state.count+1,
                 cell: cell
             })
         }
